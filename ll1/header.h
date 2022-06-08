97292531f3bfeb17655348bf6a8225213b2f20bd
varma28 <vroopa19@gmail.com>
2022-06-08 16:39:48 +0530

    -

---
+++ ll1/header.h
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#include<string.h>
+
+#include<stdlib.h>
+
+struct student
+   {
+           int a;
+           struct student *next;
+   };
+
+typedef struct student node;
+
+void  inbefore_pos(int pos,node *dummmy);
+
+node *create_node(void);
+
+void traverse(node *);
+
